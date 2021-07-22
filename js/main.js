c5ccf22297753a6010469584509ae67284b576f8
amedeo <amedeopasanisi97@gmail.com>
2021-07-22 14:16:32 +0200

    parte di esercizio vista in classe

---
+++ js/main.js
@@ -0,0 +1,22 @@
+var btnGenera = document.getElementById("calcola-prezzo");
+
+btnGenera.addEventListener('click',
+    function () {
+        var nome = getElementById("nome").value;
+        var km = getElementById("km").value;
+        var fascia-eta =getElementById(fascia-eta).value;
+
+        var biglietto = getElementById("biglietto");
+        biglietto.style.display = "block";
+    }
+)
+
+btnAnnulla.addEventListener('click',
+    function () {
+        var nome = getElementById("nome").value;
+        var km = getElementById("km").value;
+
+        var biglietto = getElementById("biglietto");
+        biglietto.style.display = "block";
+    }
+)
